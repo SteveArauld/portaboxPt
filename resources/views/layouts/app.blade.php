952cf407ebcc02e5df860a36993d5c0953bd3b71
stevearauld <stevearauld@gmail.com>
2026-07-23 15:44:30 +0100

    ajout du google tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -566,6 +566,16 @@
         });
     </script>
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17942148825"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-17942148825');
+    </script>
+
     @stack('styles')
 
 </head>
